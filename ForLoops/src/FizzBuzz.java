eb5de07259915d46fcc1bd21b9f0349d445e7d9e
Collin Paiz <imctp0205@gmail.com>
2017-12-14 11:09:45 -0800

    finished 8.1 projects. on 8.2 projects

--- ForLoops/src/FizzBuzz.java
+++ ForLoops/src/FizzBuzz.java
@@ -3,20 +3,20 @@ public class FizzBuzz
 	public static void main(String[] args)
 	{
 		for(int n = 1; n <= 100; n = n +1)
-		{
-			if(n % 3 == 0)
-			{
-				System.out.println("Fizz");
-			}
-			if(n % 5 == 0)
-			{
-				System.out.println("Buzz");
-			}
+		{			
 			if(n % 3 == 0 && n % 5 == 0)
 			{
 				System.out.println("FizzBuzz");
 			}
-			if(n % 3 != 0 && n % 5 != 0)
+			else if(n % 3 == 0)
+			{
+				System.out.println("Fizz");
+			}
+			else if(n % 5 == 0)
+			{
+				System.out.println("Buzz");
+			}
+			else
 			{
 				System.out.println(n);
 			}		
